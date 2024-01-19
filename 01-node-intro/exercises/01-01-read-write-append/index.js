e5be70bba830394c7a3915d1b0c8fa629bb350f2
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-19 10:40:25 +0700

    Revert "feat: Finish exercise-1 Read-Write-Append file on node.js"
    This reverts commit a7c693b72e2b6f6f9ab1a387bb8fc6787310e091.

--- 01-node-intro/exercises/01-01-read-write-append/index.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-fs.writeFileSync("data.txt", "Hello, World!\n");
-fs.appendFileSync("data.txt", "Second line\n");
-
-const text = fs.readFileSync("data.txt", { encoding: "utf-8" });
-
-console.log(text);
