e94d0be6706f2552d837bd8e2c2ec741448e6284
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-19 14:08:13 +0700

    feat: Finish exercise-4 Read-Write-Clear-Delete file with CLI options

--- 01-node-intro/exercises/01-04-more-cli-options/index.js
+++ 01-node-intro/exercises/01-04-more-cli-options/index.js
@@ -5,9 +5,26 @@ const filePath = "data.txt";
 const option = process.argv[2];
 const input = process.argv[3];
 
-if (option === "w") {
-  fs.appendFileSync(filePath, input + "\n");
-  console.log("appended:", input);
-} else {
-  console.log("invalid option");
+switch (option) {
+  case "w":
+    fs.appendFileSync(filePath, input + "\n");
+    console.log("appended:", input);
+    break;
+  case "r":
+    const data = fs.readFileSync(filePath, "utf-8");
+    console.log(data);
+    break;
+  case "c":
+    fs.truncateSync(filePath, 0);
+    console.log("cleared:", filePath);
+    break;
+  case "d":
+    const copy = fs.readFileSync(filePath, "utf-8");
+    fs.appendFileSync(filePath, copy, "utf-8");
+    console.log("copied content to", filePath);
+    break;
+  default:
+    console.log(
+      "Invalid option. Please use:\n w to write to file\n r to read the file\n c to clear all content on the file\n d to duplicate file content to the existing file"
+    );
 }
