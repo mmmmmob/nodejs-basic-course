a23d270ec7f7a655552c748371f7c1a84d13ec70
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-19 12:58:47 +0700

    chore: Add explaination for exercise-2

--- 01-node-intro/exercises/01-02-read-args/index.js
+++ 01-node-intro/exercises/01-02-read-args/index.js
@@ -1,8 +1,10 @@
 import fs from "fs";
 
+// import value from CLI arguments with process.argv
 const arg = process.argv;
 const text = "data.txt";
 
+// if text file doesn't exist (false is true) use writeFileSync to create a new file, else if there is a file, append new text with \n instead
 if (!text) {
   fs.writeFileSync(text, `${arg[2]}\n`);
 }
