39616d884efa1b45e77a788dcde4e2cd04da8018
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-19 10:53:30 +0700

    chore: Add explaination for exercise-1

--- 01-node-intro/exercises/01-01-read-write-append/index.js
+++ 01-node-intro/exercises/01-01-read-write-append/index.js
@@ -1,8 +1,11 @@
 import fs from "fs";
 
+// \n to enter new line
 fs.writeFileSync("data.txt", "Hello, World!\n");
-fs.appendFileSync("data.txt", "Second line\n");
+// appendFileSync to add data to the same file
+fs.appendFileSync("data.txt", "Second line");
 
+// read file w/o encoding will be showing as binary
 const text = fs.readFileSync("data.txt", { encoding: "utf-8" });
 
 console.log(text);
