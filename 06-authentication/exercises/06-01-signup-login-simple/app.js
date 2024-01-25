4e433e849008ac922776342a2ff4ec9ae3c7d5ee
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-25 10:55:30 +0700

    feat: finish Authentication - exercise 01

--- 06-authentication/exercises/06-01-signup-login-simple/app.js
+++ 06-authentication/exercises/06-01-signup-login-simple/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import morgan from "morgan";
 import helmet from "helmet";
+import morgan from "morgan";
 import { User } from "./models/user.js";
 
 const app = express();
@@ -13,44 +13,50 @@ app.use(morgan("dev"));
 
 app.post("/signup", (req, res) => {
   // 🍎 Implement: 2.1. Retrieve `email` and `password` from request.
+  const email = req.body.email;
+  const password = req.body.password;
 
   // Check if user already exists
   const existingUser = User.findOne({ email });
+
   //  🍎 Implement: 2.2. If the user already exists,
   //     return 400 error with the message "user already exists".
-  // if (existingUser) {
-  //   res.status(...).json(...);
-  //   return;
-  // }
+  if (existingUser) {
+    res.status(400).json({ error: { message: "user already exists." } });
+    return;
+  }
 
   // Store new user in database
   const user = new User({ email, password });
   user.save();
 
   //  🍎 Implement: 2.3. Return json in this format if successfully signup
-  // {
-  //   "data": {
-  //     "email": "email@sample.com"
-  //   }
-  // }
+  res.json({
+    data: {
+      email: email,
+    },
+  });
 });
 
 app.post("/login", (req, res) => {
   // 🍎 Implement: 3.1. Retrieve `email` and `password` from request.
+  const email = req.body.email;
+  const password = req.body.password;
 
   // Fetch user from database
   const user = User.findOne({ email });
+
   // 🍎 Implement: 3.2. If no user found, return 400 error with
   //    the message "Invalid email or password"
-  // if (!user) {
-  //   res.status(...).json(...);
-  //   return;
-  // }
+  if (!user) {
+    res.status(400).json({ error: { message: "user already exists." } });
+    return;
+  }
 
   // Check password
   // 🍎 Implement: 3.3. check if password is correct by
   //    comparing equal input password and user password from database.
-  const validPassword = "Implement: replace this with your code";
+  const validPassword = user.password === password;
   if (!validPassword) {
     return res
       .status(400)
